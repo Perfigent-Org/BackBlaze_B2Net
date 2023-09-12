95293ef5307a1d5c58ea8ae01946f34f64cd8598
Perfigent <113515345+Perfigent@users.noreply.github.com>
2023-09-12 11:47:46 +0530

    Update TestConstants.cs

--- B2Net.Tests/TestConstants.cs
+++ B2Net.Tests/TestConstants.cs
@@ -3,7 +3,7 @@
     public static class TestConstants {
 		public static string KeyId
 		{
-			get { return "d534cd2ca7df"; }
+			get { return ""; }
 		}
 
 		public static string AccountId {
@@ -11,11 +11,11 @@
 		}
 
 		public static string ApplicationKey {
-			get { return "0030023545b9e6247568c5852d8adf5aa735a8ee35"; }
+			get { return ""; }
 		}
 
 		public static string BucketId {
-			get { return "7d2573c42c7dc25c7ae70d1f"; }
+			get { return ""; }
 		}
 	}
 }
